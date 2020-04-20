5872a9f974fcbe2cd120b22a821cb89f964d12ea
PlasmaIntec <alphaintec@gmail.com>
2020-04-20 16:04:47 -0700

    Temporary patch: add column name line to csv's later

--- printFileInDirectory.ino
+++ printFileInDirectory.ino
@@ -2,6 +2,7 @@ void printFileInDirectory(String dirName, EthernetClient client) {
 	File myDirectory = SD.open(dirName);
 	File myFile = myDirectory.openNextFile();
 	if (myFile) {
+		client.println("time,tankid,temp,temp setpoint,pH,pH setpoint,onTime,Kp,Ki,Kd");
 		// read from the file until there's nothing else in it:
 		while (myFile.available()) {
 			client.write(myFile.read());

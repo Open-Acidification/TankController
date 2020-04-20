a755d84c6b3e0fc389d3e9bc6f9a5082f22341f0
PlasmaIntec <alphaintec@gmail.com>
2020-04-19 21:31:42 -0700

    Remove handleMisc input pin reading to diagnose whether device is resetting

--- handleMisc.ino
+++ handleMisc.ino
@@ -7,13 +7,13 @@ void handleMisc(EthernetClient client) {
 	client.println("<!DOCTYPE HTML>");
 	client.println("<html>");
 	// output the value of each analog input pin
-	for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-		int sensorReading = analogRead(analogChannel);
-		client.print("analog input ");
-		client.print(analogChannel);
-		client.print(" is ");
-		client.print(sensorReading);
-		client.println("<br />");
-	}
+	// for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+	// 	int sensorReading = analogRead(analogChannel);
+	// 	client.print("analog input ");
+	// 	client.print(analogChannel);
+	// 	client.print(" is ");
+	// 	client.print(sensorReading);
+	// 	client.println("<br />");
+	// }
 	client.println("</html>");
 }

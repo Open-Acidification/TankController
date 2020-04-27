d127601a6d1efbc9760427b5502b26c13cf6c717
PlasmaIntec <alphaintec@gmail.com>
2020-04-27 14:31:34 -0700

    Add attribution

--- fmtDouble.ino
+++ fmtDouble.ino
@@ -1,3 +1,4 @@
+// https://forum.arduino.cc/index.php/topic,44216.0.html#11
 void fmtDouble(double val, byte precision, char *buf, unsigned bufLen = 0xffff);
 unsigned fmtUnsigned(unsigned long val, char *buf, unsigned bufLen = 0xffff, byte width = 0);
 

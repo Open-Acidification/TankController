954f9eea06ca27460e8680104a6b8db6fb9e87ab
PlasmaIntec <alphaintec@gmail.com>
2020-04-27 15:04:02 -0700

    Experiement with recursive DynamicJsonDocument

--- printCurrentLevelDirectories.ino
+++ printCurrentLevelDirectories.ino
@@ -1,22 +1,26 @@
-void printCurrentLevelDirectories(String dirName, EthernetClient client) {
+DynamicJsonDocument printCurrentLevelDirectories(String dirName, EthernetClient client) {
 	File dir = SD.open(dirName);
 
 	DynamicJsonDocument doc(1024);
-	JsonArray coords = doc.createNestedArray("files");
-	coords.add(48.7507371);
-	coords.add(2.2625587);
-	serializeJson(doc, client);
+	JsonArray filesArray = doc.createNestedArray("files");
+	filesArray.add(48.7507371);
+	filesArray.add(2.2625587);
 	while (true) {
 
 		File entry =  dir.openNextFile();
 		if (!entry) {
-		// no more files
-		break;
+			// no more files
+			break;
 		}
 		char* hasLetterS = strchr(entry.name(), 'S');
 		if (entry.isDirectory() && !hasLetterS) { // WILL NOT PRINT SYSTEM~1 AT ROOT LEVEL
 			client.println(entry.name());
+			DynamicJsonDocument directories = printCurrentLevelDirectories(dirName + "/" + entry.name(), client);
+			filesArray.add(directories);
 		}
 		entry.close();
 	}
+	serializeJson(doc, client);
+	client.println();
+	return doc;
 }

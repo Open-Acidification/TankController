02b33b435394458be65f6a794a069cd22326e355
PlasmaIntec <alphaintec@gmail.com>
2020-04-27 14:43:13 -0700

    Experiment with DynamicJsonBuffer

--- printCurrentLevelDirectories.ino
+++ printCurrentLevelDirectories.ino
@@ -1,5 +1,12 @@
 void printCurrentLevelDirectories(String dirName, EthernetClient client) {
 	File dir = SD.open(dirName);
+
+	DynamicJsonBuffer jsonBuffer;
+	JsonObject& root = jsonBuffer.createObject();
+	JsonArray& coords = root.createNestedArray("files");
+	coords.add(48.7507371, 7);
+	coords.add(2.2625587);
+	root.prettyPrintTo(client);
 	while (true) {
 
 		File entry =  dir.openNextFile();

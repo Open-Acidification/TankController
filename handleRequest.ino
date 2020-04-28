4efd109d6070c15110294b64a48aec98fa440cd3
PlasmaIntec <alphaintec@gmail.com>
2020-04-28 08:45:15 -0700

    Add mac endpoint

--- handleRequest.ino
+++ handleRequest.ino
@@ -79,6 +79,12 @@ void handleRequest(EthernetClient client) {
       client.println();
       File root = SD.open("/");
       printDirectoryToClient(root, 0, client);
+    } else if (endpoint.startsWith("/mac")) {
+      client.println("HTTP/1.1 200 OK");
+      client.println("Content-Type: text/plain");
+      client.println("Connection: close"); 
+      client.println();
+      client.println(macstr);
     } else if (endpoint.startsWith("/time")) {
       client.println("HTTP/1.1 200 OK");
       client.println("Content-Type: text/plain");

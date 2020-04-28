81d006df092fe1bc4125545a91e11c3d1ca0dbb5
PlasmaIntec <alphaintec@gmail.com>
2020-04-27 22:43:19 -0700

    Use keeo-alive

--- handleData.ino
+++ handleData.ino
@@ -1,7 +1,7 @@
 void handleData(String endpoint, EthernetClient client) {	  
 	client.println("HTTP/1.1 200 OK");
 	client.println("Content-Type: text/plain; charset=UTF-8");
-	client.println("Connection: close");  // the connection will be closed after completion of the response
+	client.println("Connection: keep-alive");  // the connection will be closed after completion of the response
 	// data endpoint format: data/year/month/day/hour
 	String directoryName;
 	if (endpoint.length() >= 5) {

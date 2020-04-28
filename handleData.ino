a5545c523f0eaef7e3da7e5a1269a72b19ed0c37
PlasmaIntec <alphaintec@gmail.com>
2020-04-27 22:17:37 -0700

    Add doc

--- handleData.ino
+++ handleData.ino
@@ -23,19 +23,19 @@ void handleData(String endpoint, EthernetClient client) {
 	File dir = SD.open(directoryName);
 	switch (slashes) {
 		case 0: // data: return years available
-			StaticJsonDocument<512> = printCurrentLevelDirectories(dir, client, slashes);
+			StaticJsonDocument<512> doc = printCurrentLevelDirectories(dir, client, slashes);
 			serializeJson(doc, client);
 			break;
 		case 1: // year: return months available
-			StaticJsonDocument<512> = printCurrentLevelDirectories(dir, client, slashes);
+			StaticJsonDocument<512> doc = printCurrentLevelDirectories(dir, client, slashes);
 			serializeJson(doc, client);
 			break;
 		case 2: // month: return days available
-			StaticJsonDocument<512> = printCurrentLevelDirectories(dir, client, slashes);
+			StaticJsonDocument<512> doc = printCurrentLevelDirectories(dir, client, slashes);
 			serializeJson(doc, client);
 			break;
 		case 3: // day: return hours available
-			StaticJsonDocument<512> = printCurrentLevelDirectories(dir, client, slashes);
+			StaticJsonDocument<512> doc = printCurrentLevelDirectories(dir, client, slashes);
 			serializeJson(doc, client);
 			break;
 		case 4: // hour: return csv of the specific hour

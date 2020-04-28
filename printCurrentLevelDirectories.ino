142ba5ef1916560fa7591314f1fb563e7ef38dd7
PlasmaIntec <alphaintec@gmail.com>
2020-04-27 17:36:08 -0700

    Restructure logic again

--- printCurrentLevelDirectories.ino
+++ printCurrentLevelDirectories.ino
@@ -1,33 +1,27 @@
 JsonDocument printCurrentLevelDirectories(File dir, EthernetClient client, int level) {
-	// File dir = SD.open(dirName);
-
-	// DynamicJsonDocument tempDoc(1024);
-	// DynamicJsonDocument nestedDoc(1024);
-	// JsonArray tempArray = nestedDoc.createNestedArray("tempArray");
-	// tempArray.add(1);
-	// tempArray.add(2);
-	// tempArray.add(3);
-	// JsonArray tempOuterArray = tempDoc.createNestedArray("tempOuterArray");
-	// tempOuterArray.add(nestedDoc);
-	// serializeJson(tempDoc, client);
-
 	StaticJsonDocument<1024> doc;
 	client.println(dir.name());
 	client.println("CURRENT LEVEL:");
 	client.println(level);
 	JsonArray filesArray = doc.createNestedArray(dir.name());
-	if (level < 3) {
-		while (true) {
+	while (true) {
 
-			File entry =  dir.openNextFile();
-			if (!entry) {
-				// no more files
-				break;
-			}
-			char* hasLetterS = strchr(entry.name(), 'S');
-			if (entry.isDirectory() && !hasLetterS) { // WILL NOT PRINT SYSTEM~1 AT ROOT LEVEL
-				client.println(entry.name());
-				// printCurrentLevelDirectories(dirName + "/" + entry.name(), client);
+		File entry =  dir.openNextFile();
+		if (!entry) {
+			// no more files
+			break;
+		}
+		char* hasLetterS = strchr(entry.name(), 'S');
+		if (entry.isDirectory() && !hasLetterS) { // WILL NOT PRINT SYSTEM~1 AT ROOT LEVEL
+			client.println(entry.name());
+			// printCurrentLevelDirectories(dirName + "/" + entry.name(), client);
+			if (level == 3) {
+				while (true) {
+					File leafEntry = dir.openNextFile();
+					if (!entry) break;
+					filesArray.add(leafEntry.name());
+				}
+			} else {
 				StaticJsonDocument<1024> directories = printCurrentLevelDirectories(entry, client, level+1);
 				client.println("DIRECTORY");
 				serializeJson(doc, client);
@@ -36,10 +30,8 @@ JsonDocument printCurrentLevelDirectories(File dir, EthernetClient client, int l
 				client.println("POST DIRECTORY");
 				filesArray.add(directories);
 			}
-			entry.close();
 		}
-	} else {
-		filesArray.add(dir.name());
+		entry.close();
 	}
 	serializeJson(doc, client);
 	client.println();

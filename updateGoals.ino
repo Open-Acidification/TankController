f09a9a8aa4b4c72961c52cfcde5d757fcad55a95
PlasmaIntec <alphaintec@gmail.com>
2020-05-18 23:17:04 -0700

    Remove debugging from updateGoals

--- updateGoals.ino
+++ updateGoals.ino
@@ -1,23 +1,10 @@
 void updateGoals() {
 	long now = millis();
 
-	Serial.print("UPDATING GOALS AT MILLIS: ");
-	Serial.println(now);
-	Serial.print("phInterval: ");
-	Serial.println(phInterval);
-
 	long phTime = now % phInterval;
 
-	Serial.print("phTime: ");
-	Serial.println(phTime);
-	
 	long phTimeCurrent = readLineFromSD("pt.txt", phSeriesPointer, goalRecordLength);
 	long phTimeNext = readLineFromSD("pt.txt", phSeriesPointer+1 >= phSeriesSize ? 0 : phSeriesPointer+1, goalRecordLength);
-
-	Serial.print("phTimeCurrent: ");
-	Serial.println(phTimeCurrent);
-	Serial.print("phTimeNext: ");
-	Serial.println(phTimeNext);
 
 	// we want to make sure phTime is always between phTimeCurrent and phTimeNext
 	if (phSeriesSize) {
@@ -28,23 +15,11 @@ void updateGoals() {
 			}
 			phTimeCurrent = readLineFromSD("pt.txt", phSeriesPointer, goalRecordLength);
 			phTimeNext = readLineFromSD("pt.txt", phSeriesPointer+1 >= phSeriesSize ? 0 : phSeriesPointer+1, goalRecordLength);
-			Serial.print("phTimeCurrent: ");
-			Serial.println(phTimeCurrent);
-			Serial.print("phTimeNext: ");
-			Serial.println(phTimeNext);
-			Serial.print("phSeriesPointer: ");
-			Serial.println(phSeriesPointer);
 		}
 		if ((phTimeNext < phTimeCurrent) && (phTimeCurrent > phTime)) {
 			phSeriesPointer = 0;
 			phTimeCurrent = readLineFromSD("pt.txt", phSeriesPointer, goalRecordLength);
 			phTimeNext = readLineFromSD("pt.txt", phSeriesPointer+1 >= phSeriesSize ? 0 : phSeriesPointer+1, goalRecordLength);
-			Serial.print("phTimeCurrent: ");
-			Serial.println(phTimeCurrent);
-			Serial.print("phTimeNext: ");
-			Serial.println(phTimeNext);
-			Serial.print("phSeriesPointer: ");
-			Serial.println(phSeriesPointer);
 		}
 	}
 
@@ -58,29 +33,12 @@ void updateGoals() {
 		phValueNext = readLineFromSD("pv.txt", phSeriesPointer+1, goalRecordLength);
 	}
 	phset = phValueCurrent + (phValueNext - phValueCurrent) * (phTime - phTimeCurrent) / (phTimeNext - phTimeCurrent);
-	Serial.print("phValueCurrent: ");
-	Serial.println(phValueCurrent);
-	Serial.print("phValueNext: ");
-	Serial.println(phValueNext);
-	Serial.print("phset: ");
-	Serial.println(phset);
 
 	// now do the same for temp
-	Serial.print("tempInterval: ");
-	Serial.println(tempInterval);
-
 	long tempTime = now % tempInterval;
 
-	Serial.print("tempTime: ");
-	Serial.println(tempTime);
-	
 	long tempTimeCurrent = readLineFromSD("tt.txt", tempSeriesPointer, goalRecordLength);
 	long tempTimeNext = readLineFromSD("tt.txt", tempSeriesPointer+1 >= tempSeriesSize ? 0 : tempSeriesPointer+1, goalRecordLength);
-
-	Serial.print("tempTimeCurrent: ");
-	Serial.println(tempTimeCurrent);
-	Serial.print("tempTimeNext: ");
-	Serial.println(tempTimeNext);
 
 	// we want to make sure tempTime is always between tempTimeCurrent and tempTimeNext
 	if (tempSeriesSize) {
@@ -91,23 +49,11 @@ void updateGoals() {
 			}
 			tempTimeCurrent = readLineFromSD("tt.txt", tempSeriesPointer, goalRecordLength);
 			tempTimeNext = readLineFromSD("tt.txt", tempSeriesPointer+1 >= tempSeriesSize ? 0 : tempSeriesPointer+1, goalRecordLength);
-			Serial.print("tempTimeCurrent: ");
-			Serial.println(tempTimeCurrent);
-			Serial.print("tempTimeNext: ");
-			Serial.println(tempTimeNext);
-			Serial.print("tempSeriesPointer: ");
-			Serial.println(tempSeriesPointer);
 		}
 		if ((tempTimeNext < tempTimeCurrent) && (tempTimeCurrent > tempTime)) {
 			tempSeriesPointer = 0;
 			tempTimeCurrent = readLineFromSD("tt.txt", tempSeriesPointer, goalRecordLength);
 			tempTimeNext = readLineFromSD("tt.txt", tempSeriesPointer+1 >= tempSeriesSize ? 0 : tempSeriesPointer+1, goalRecordLength);
-			Serial.print("tempTimeCurrent: ");
-			Serial.println(tempTimeCurrent);
-			Serial.print("tempTimeNext: ");
-			Serial.println(tempTimeNext);
-			Serial.print("tempSeriesPointer: ");
-			Serial.println(tempSeriesPointer);
 		}
 	}
 
@@ -121,10 +67,4 @@ void updateGoals() {
 		tempValueNext = readLineFromSD("tv.txt", tempSeriesPointer+1, goalRecordLength);
 	}
 	tempset = tempValueCurrent + (tempValueNext - tempValueCurrent) * (tempTime - tempTimeCurrent) / (tempTimeNext - tempTimeCurrent);
-	Serial.print("tempValueCurrent: ");
-	Serial.println(tempValueCurrent);
-	Serial.print("tempValueNext: ");
-	Serial.println(tempValueNext);
-	Serial.print("tempset: ");
-	Serial.println(tempset);
 }

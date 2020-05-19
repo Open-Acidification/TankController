a59ee3b9fb478b3a235059cf1660f152d100c181
PlasmaIntec <alphaintec@gmail.com>
2020-05-18 23:35:48 -0700

    Adapt refresh script

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,6 @@
+git checkout -- .
+git checkout remote_testing
 git pull
+git checkout origin/remote_testing
 arduino-cli compile -b arduino:avr:mega /home/matma/Open-Acidification
 arduino-cli upload /home/matma/Open-Acidification -b arduino:avr:mega -p /dev/ttyACM0

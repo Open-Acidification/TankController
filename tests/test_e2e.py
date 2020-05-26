7bf723517358acd187962112547cfce460dd56d2
PlasmaIntec <alphaintec@gmail.com>
2020-05-25 23:25:57 -0700

    Test e2e get ARDUINO_IP_ADDRESS

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -2,6 +2,15 @@ import requests
 import os
 
 def test_e2e():
-	print(os.environ)
 	print(os.environ["ARDUINO_IP_ADDRESS"])
+
+	response = requests.get(
+		os.environ["ARDUINO_IP_ADDRESS"]
+	)
+	
+	print(response.status_code)
+	print(response.content.decode().replace('\r', '\n'))
+	
+	# sys.stdout.write(response.text)
+	# print(response.text, end='\r')
 	assert True

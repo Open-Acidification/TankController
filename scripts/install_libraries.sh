38955f2792d5e752b88dde1373f354a3e71b83ee
Mikhail Beresnev <57458750+Mikhail-Beresnev@users.noreply.github.com>
2020-08-05 15:24:42 -0400

    trying to change name

--- scripts/install_libraries.sh
+++ scripts/install_libraries.sh
@@ -1,5 +1,5 @@
 arduino-cli lib install "Adafruit Keypad"@1.0.4
-arduino-cli lib install "Adafruit MAX31865"@1.0.3
+arduino-cli lib install "Adafruit MAX31865 library"@1.0.3
 arduino-cli lib install ArduinoJson@6.14.1
 arduino-cli lib install AsyncTaskLib@1.0.0
 arduino-cli lib install Ethernet@2.0.0

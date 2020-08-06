47272de0604305ba71bfbdd817659f990b13905c
Mikhail Beresnev <57458750+Mikhail-Beresnev@users.noreply.github.com>
2020-08-06 13:47:07 -0400

    Adding constants to variables visibly constant

--- Open_Acidification_pH-stat_arduino/Open_Acidification_pH-stat_arduino.ino
+++ Open_Acidification_pH-stat_arduino/Open_Acidification_pH-stat_arduino.ino
@@ -34,16 +34,16 @@
 #include <string.h>
 #include "RTClib.h"
 
-String DevID = "v172D35C152EDA6C";  // DeviceID from Pushingbox
+const String DevID = "v172D35C152EDA6C";  // DeviceID from Pushingbox
 
-#define RREF 430.0
+const float RREF = 430.0;
 Adafruit_MAX31865 max = Adafruit_MAX31865(45, 43, 41, 39);
 RTC_PCF8523 rtc;
 
-double softvers = 0.197;  // Software Version
+const double softvers = 0.197;  // Software Version
 
 // byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //Setting MAC Address
-char APIServer[] = "api.pushingbox.com";  // pushingbox API server
+const char APIServer[] = "api.pushingbox.com";  // pushingbox API server
 IPAddress ip(192, 168, 1, 2);             // Arduino IP address. Only used when DHCP is turned off.
 EthernetClient client;                    // define 'client' as object
 EthernetServer ethernetServer(80);
@@ -55,24 +55,24 @@ float interval = 1200000;  // interval at which to update Google Sheets (millise
 unsigned long previousMillis =
     0 - interval + 30000;  // will store last time Google Sheets was updated (-interval+30000 sets first upload for 30 seconds after startup. This eases troubleshooting)
 unsigned long chiller_previousMillis = 0;  // will store last time chiller state was checked
-float chiller_interval = 30000;            // interval at which to change chiller state (milliseconds)
+const float chiller_interval = 30000;            // interval at which to change chiller state (milliseconds)
 // float SD_interval = 86400000;                     // interval at which to start a new log file (milliseconds) (formerly 1 day)
-float SD_interval = 3600000;              // log to SD each hour
+const float SD_interval = 3600000;              // log to SD each hour
 unsigned long sensor_previousMillis = 0;  // will store last time sensor readings were taken
 float sensor_interval;                    // interval at which to start a new log file (milliseconds)
 unsigned long second_previousMillis = 0;
-float second_interval = 1000;
+const float second_interval = 1000;
 int granularity;
 int maxDataAge;
-float LeaseInterval = 4 * 86400000;  // Interval at which to renew DHCP lease (First number is days)
+const float LeaseInterval = 4 * 86400000;  // Interval at which to renew DHCP lease (First number is days)
 unsigned long previousLease = 0;
 
 const byte ROWS = 4;       // four rows
 const byte COLS = 4;       // four columns
 String inputstring = "";   // a string to hold incoming data from the PC
 String sensorstring = "";  // a string to hold the data from the EZO pH circuit
-String premidcalstring = "Cal,mid,";
-String prelowcalstring = "Cal,low,";
+const String premidcalstring = "Cal,mid,";
+const String prelowcalstring = "Cal,low,";
 String midcalstring = "";
 String lowcalstring = "";
 String rightnow;
@@ -105,8 +105,8 @@ double Kd;
 double amplitude;
 double frequency;
 long onTime = 0;
-int recordLength = 70;   // length of each record to be logged
-int WindowSize = 10000;  // 10 second Time Proportional Output window
+const int recordLength = 70;   // length of each record to be logged
+const int WindowSize = 10000;  // 10 second Time Proportional Output window
 unsigned long windowStartTime;
 float midBuffer = 0;
 float lowBuffer = 0;
@@ -120,10 +120,10 @@ byte mac[6] = {0x90, 0xA2, 0xDA, 0x00, 0x00, 0x00};
 char macstr[18];
 boolean pidrun = true;
 boolean sensed = false;
-#define chiller 47
-#define co2reg 49
+const int chiller = 47;
+const int co2reg = 49;
 int LoopStart;
-int goalRecordLength = 10;
+const int goalRecordLength = 10;
 long phInterval;
 long phDelay;
 int phSeriesSize;
@@ -133,7 +133,7 @@ long tempDelay;
 int tempSeriesSize;
 long tempSeriesPointer;
 
-int const htmlRequestBufferSize = 500;
+const int htmlRequestBufferSize = 500; (prev was int const)
 char htmlRequestBuffer[htmlRequestBufferSize];
 
 // Temperature Smoothing/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -173,9 +173,9 @@ const int tempDelayAddress = 100;
 String filename;
 String file_full;
 
-char stdKeys[ROWS][COLS] = {{'1', '2', '3', 'A'}, {'4', '5', '6', 'B'}, {'7', '8', '9', 'C'}, {'*', '0', '#', 'D'}};
-byte rowPins[ROWS] = {34, 36, 38, 40};
-byte colPins[COLS] = {42, 44, 46, 48};
+const char stdKeys[ROWS][COLS] = {{'1', '2', '3', 'A'}, {'4', '5', '6', 'B'}, {'7', '8', '9', 'C'}, {'*', '0', '#', 'D'}};
+const byte rowPins[ROWS] = {34, 36, 38, 40};
+const byte colPins[COLS] = {42, 44, 46, 48};
 Keypad customKeypad = Keypad(makeKeymap(stdKeys), rowPins, colPins, ROWS, COLS);
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);  // Starting the PID, Specify the links and initial tuning parameters
 File myFile;
@@ -187,9 +187,9 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 // ************************************************
 byte ATuneModeRemember = 2;
 
-double aTuneStep = 500;
-double aTuneNoise = 1;
-unsigned int aTuneLookBack = 20;
+const double aTuneStep = 500;
+const double aTuneNoise = 1;
+const unsigned int aTuneLookBack = 20;
 
 boolean tuning = false;
 
@@ -203,21 +203,21 @@ unsigned int localPort = 8888;  // local port to listen for UDP packets
 // End Time////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Custom Characters for splash////////////////////////////////
-byte ol1[8] = {0b00000, 0b00000, 0b00001, 0b00011, 0b00011, 0b00111, 0b00111, 0b00110};
+const byte ol1[8] = {0b00000, 0b00000, 0b00001, 0b00011, 0b00011, 0b00111, 0b00111, 0b00110};
 
-byte ol2[8] = {0b00111, 0b11111, 0b11111, 0b10011, 0b11101, 0b11110, 0b00101, 0b11011};
+const byte ol2[8] = {0b00111, 0b11111, 0b11111, 0b10011, 0b11101, 0b11110, 0b00101, 0b11011};
 
-byte ol3[8] = {0b11100, 0b11111, 0b01111, 0b10111, 0b10110, 0b01110, 0b10101, 0b11011};
+const byte ol3[8] = {0b11100, 0b11111, 0b01111, 0b10111, 0b10110, 0b01110, 0b10101, 0b11011};
 
-byte ol4[8] = {0b00000, 0b00000, 0b10000, 0b11000, 0b11000, 0b11100, 0b11100, 0b11100};
+const byte ol4[8] = {0b00000, 0b00000, 0b10000, 0b11000, 0b11000, 0b11100, 0b11100, 0b11100};
 
-byte ol5[8] = {0b00111, 0b00111, 0b00111, 0b00011, 0b00011, 0b00001, 0b00000, 0b00000};
+const byte ol5[8] = {0b00111, 0b00111, 0b00111, 0b00011, 0b00011, 0b00001, 0b00000, 0b00000};
 
-byte ol6[8] = {0b11011, 0b10101, 0b01110, 0b01101, 0b11101, 0b11110, 0b11111, 0b00111};
+const byte ol6[8] = {0b11011, 0b10101, 0b01110, 0b01101, 0b11101, 0b11110, 0b11111, 0b00111};
 
-byte ol7[8] = {0b11011, 0b10100, 0b01111, 0b10111, 0b11001, 0b11111, 0b11111, 0b11100};
+const byte ol7[8] = {0b11011, 0b10100, 0b01111, 0b10111, 0b11001, 0b11111, 0b11111, 0b11100};
 
-byte ol8[8] = {0b01100, 0b11100, 0b11100, 0b11000, 0b11000, 0b10000, 0b00000, 0b00000};
+const byte ol8[8] = {0b01100, 0b11100, 0b11100, 0b11000, 0b11000, 0b10000, 0b00000, 0b00000};
 
 void serialEvent() {                         // if the hardware serial port_0 receives a char
   inputstring = Serial.readStringUntil(13);  // read the string until we see a <CR>

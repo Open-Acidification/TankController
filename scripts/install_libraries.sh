94495390060c344ae5164b431aff13f6a4b91f45
Preston Carman <prestonc@apache.org>
2020-09-16 17:16:38 -0700

    Removed standard libraries.

--- scripts/install_libraries.sh
+++ scripts/install_libraries.sh
@@ -1,13 +1,15 @@
+# Standard libraries being used include:
+#  > EEPROM, Ethernet, LiquidCrystal, SD, SPI, Wire
+#
+# Note these come pre-installed.
+
 # These libraries are to be installed with the arduino-cli tool
 # All libraries should include a version number
 arduino-cli lib install "Adafruit MAX31865 library"@1.0.3
 arduino-cli lib install ArduinoJson@6.14.1
-arduino-cli lib install Ethernet@2.0.0
 arduino-cli lib install Keypad@3.1.1
-arduino-cli lib install LiquidCrystal@1.0.7
 arduino-cli lib install PID@1.2.0
 arduino-cli lib install RTClib@1.3.3
-arduino-cli lib install SD@1.2.4
 
 # These libraries are to be installed via git clone
 # These libraries could not be found in the arduino-cli library list

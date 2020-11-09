f9672d0d82d1df406a52932d384215d8db036b77
James Foster <github@jgfoster.net>
2020-11-09 11:50:58 -0800

    Explicitly install libraries (#18)

--- scripts/install_libraries.sh
+++ scripts/install_libraries.sh
@@ -7,3 +7,14 @@ bundle exec ensure_arduino_installation.rb
 git clone https://github.com/Arduino-CI/LiquidCrystal.git
 mv LiquidCrystal $(bundle exec arduino_library_location.rb)
 
+# get custom version of RTClib
+git clone https://github.com/Arduino-CI/RTClib.git
+mv RTClib $(bundle exec arduino_library_location.rb)
+
+# get custom version of Adafruit_BusIO
+git clone https://github.com/Arduino-CI/Adafruit_BusIO.git
+mv Adafruit_BusIO $(bundle exec arduino_library_location.rb)
+
+# get custom version of Adafruit_MAX31865
+git clone https://github.com/Arduino-CI/Adafruit_MAX31865.git
+mv Adafruit_MAX31865 $(bundle exec arduino_library_location.rb)

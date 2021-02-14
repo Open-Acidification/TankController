d29876401ddf942c744e09ad87db591074a7b866
eucalvo <71796520+eucalvo@users.noreply.github.com>
2021-02-13 19:59:04 -0800

    add TemperatureControl and tests (#48)
    * add TemperatureControl and tests
    
    * fixed formatting
    
    * remove blank line
    
    * cleaned up comments and readability
    
    * formatted documents
    
    Co-authored-by: calvel <calvel@cs.wallawalla.edu>

---
+++ src/Devices/TemperatureControl.h
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+
+/**
+ * Temperature Control
+ * Controlling the tank temperature is done by external devices that are controlled by setting
+ * designated pins on the Arduino.
+ */
+
+class TemperatureControl {
+protected:
+  const int PIN = 47;
+  const double DELTA = 0.05;
+  double targetTemperature;
+  bool currentSwitchState = false;
+
+  void turnOn(bool state);
+
+public:
+  TemperatureControl() {
+  }
+  void setTargetTemperature(double newTemperature) {
+    targetTemperature = newTemperature;
+  }
+  virtual void updateControl(double currentTemperature) = 0;
+  bool getCurrentSwitchState() {
+    return currentSwitchState;
+  }
+};
+
+class Heater : public TemperatureControl {
+private:
+public:
+  void updateControl(double currentTemperature);
+};
+
+class Chiller : public TemperatureControl {
+private:
+  const unsigned long TIME_INTERVAL = 30 * 1000UL;  // interval at which to change chiller state
+  unsigned long previousMillis = 0;                 // will store last time chiller state was checked
+
+public:
+  void updateControl(double currentTemperature);
+};

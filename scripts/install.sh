3077f6ab2d5f604d0aac03c7be6d7919c5f45e89
Wesley <wesleypacs@gmail.com>
2021-03-26 11:20:55 -0700

    Updated install scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,8 +4,8 @@ sudo apt install -y \
   build-essential git ruby libgtk-3-dev \
   python3-dev python3-pip python-is-python3
 sudo gem install bundler
-git clone https://github.com/Open-Acidification/TankControllerLib.git
-cd TankControllerLib
+git clone https://github.com/Open-Acidification/TankController.git
+cd TankController
 bundle config --local path vendor/bundle
 bundle install
 scripts/install_libraries.sh
--- scripts/install_libraries.sh
+++ scripts/install_libraries.sh
@@ -9,9 +9,6 @@ export SHALLOW_MASTER='--depth 1 --branch master --single-branch '
 # use this if you started with SHALLOW and now want the full repository with history
 # git fetch --unshallow
 
-# Use for installing locally
-# cd $(bundle exec arduino_library_location.rb)
-
 if ! [ $(id -u) = 0 ]; then
   bundle config --local path vendor/bundle
   bundle install
@@ -22,55 +19,82 @@ fi
 # add missing libraries
 export GITHUB="https://github.com/Arduino-CI"
 if [ -d "./Adafruit_BusIO" ]; then
-  (cd Adafruit_BusIO; git pull)
+  (
+    cd Adafruit_BusIO
+    git pull
+  )
 else
   git clone $SHALLOW_MASTER $GITHUB/Adafruit_BusIO.git
 fi
 
 if [ -d "./Adafruit_MAX31865" ]; then
-  (cd Adafruit_MAX31865; git pull)
+  (
+    cd Adafruit_MAX31865
+    git pull
+  )
 else
   git clone $SHALLOW_MAIN $GITHUB/Adafruit_MAX31865.git
 fi
 
 if [ -d "./Ethernet" ]; then
-  (cd Ethernet; git pull)
+  (
+    cd Ethernet
+    git pull
+  )
 else
   git clone $SHALLOW_MASTER $GITHUB/Ethernet.git
 fi
 
 if [ -d "./LiquidCrystal" ]; then
-  (cd LiquidCrystal; git pull)
+  (
+    cd LiquidCrystal
+    git pull
+  )
 else
   git clone $SHALLOW_MASTER $GITHUB/LiquidCrystal.git
 fi
 
 if [ -d "./RTClib" ]; then
-  (cd RTClib; git pull)
+  (
+    cd RTClib
+    git pull
+  )
 else
   git clone $SHALLOW_MASTER $GITHUB/RTClib.git
 fi
 
 if [ -d "./Keypad" ]; then
-  (cd Keypad; git pull)
+  (
+    cd Keypad
+    git pull
+  )
 else
   git clone $SHALLOW_MASTER $GITHUB/Keypad.git
 fi
 
 if [ -d "./Arduino-PID-Library" ]; then
-  (cd Arduino-PID-Library; git pull)
+  (
+    cd Arduino-PID-Library
+    git pull
+  )
 else
   git clone $SHALLOW_MASTER $GITHUB/Arduino-PID-Library.git
 fi
 
 if [ -d "./Arduino-PID-AutoTune-Library" ]; then
-  (cd Arduino-PID-AutoTune-Library; git pull)
+  (
+    cd Arduino-PID-AutoTune-Library
+    git pull
+  )
 else
   git clone $SHALLOW_MASTER $GITHUB/Arduino-PID-AutoTune-Library.git
 fi
 
 if [ -d "./SD" ]; then
-  (cd SD; git pull)
+  (
+    cd SD
+    git pull
+  )
 else
   git clone $SHALLOW_MAIN $GITHUB/SD.git
 fi

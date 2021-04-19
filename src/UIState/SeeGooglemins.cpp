00033c2827a7ccedc84b37a3dfeeca0ba985cdac
Wesley <wesleypacs@gmail.com>
2021-04-18 20:52:14 -0700

    SeeGoogleMins

---
+++ src/UIState/SeeGooglemins.cpp
@@ -0,0 +1,14 @@
+/**
+ * SeeGoogleMins.cpp
+ */
+
+#include "SeeGoogleMins.h"
+
+#include "../Devices/EEPROM_TC.h"
+#include "../Devices/LiquidCrystal_TC.h"
+
+void SeeGoogleMins::loop() {
+  char buffer[17];
+  sprintf(buffer, "%i", EEPROM_TC::instance()->getGoogleSheetInterval() / 60000);
+  LiquidCrystal_TC::instance()->writeLine(buffer, 1);
+}
---
+++ test/SeeGoogleMins.cpp
@@ -0,0 +1,32 @@
+#include "SeeGoogleMins.h"
+
+#include <Arduino.h>
+#include <ArduinoUnitTests.h>
+
+#include "EEPROM_TC.h"
+#include "Keypad_TC.h"
+#include "LiquidCrystal_TC.h"
+#include "TankControllerLib.h"
+
+unittest(testOutput) {
+  TankControllerLib* tc = TankControllerLib::instance();
+  LiquidCrystal_TC* display = LiquidCrystal_TC::instance();
+  assertEqual("MainMenu", tc->stateName());
+  SeeGoogleMins* test = new SeeGoogleMins(tc);
+  tc->setNextState(test, true);
+  assertEqual("SeeGoogleMins", tc->stateName());
+
+  // Test the output
+  double prevInterval = EEPROM_TC::instance()->getGoogleSheetInterval();
+  EEPROM_TC::instance()->setGoogleSheetInterval(12000060);
+  tc->loop();
+  assertEqual("Google Mins:    ", display->getLines().at(0));
+  assertEqual("200             ", display->getLines().at(1));
+  // Return to mainMenu
+  Keypad_TC::instance()->_getPuppet()->push_back('D');
+  tc->loop();
+  assertEqual("MainMenu", tc->stateName());
+  EEPROM_TC::instance()->setGoogleSheetInterval(prevInterval);
+}
+
+unittest_main()

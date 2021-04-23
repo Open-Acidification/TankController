e84f047b4a5b881743fd3c181e8ffa7e32811684
calvel <calvel@cs.wallawalla.edu>
2021-04-23 02:15:11 +0000

    add needed parameters

--- src/UIState/PHCalibrationLow.cpp
+++ src/UIState/PHCalibrationLow.cpp
@@ -13,7 +13,7 @@ void PHCalibrationLow::setValue(double value) {
   PHProbe::instance()->setLowpointCalibration(value);
 
   char output[17];
-  snprintf(output, "New Low = %2.3f", value);  // "New Low = 12.345"
+  snprintf(output, sizeof(output), "New Low = %2.3f", value);  // "New Low = 12.345"
   LiquidCrystal_TC::instance()->writeLine(output, 1);
   this->setNextState((UIState*)new Wait(tc, 1000, new PHCalibrationHigh(tc)));
 }
--- src/UIState/PHCalibrationMid.cpp
+++ src/UIState/PHCalibrationMid.cpp
@@ -13,7 +13,7 @@ void PHCalibrationMid::setValue(double value) {
   PHProbe::instance()->setMidpointCalibration(value);
 
   char output[17];
-  snprintf(output, "New Mid = %2.3f", value);  // "New Mid = 12.345"
+  snprintf(output, sizeof(output), "New Mid = %2.3f", value);  // "New Mid = 12.345"
   LiquidCrystal_TC::instance()->writeLine(output, 1);
   this->setNextState((UIState*)new Wait(tc, 1000, new PHCalibrationLow(tc)));
 }

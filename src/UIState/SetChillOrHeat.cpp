acec0b811f8f5ca2105b886d7231a7b0db36f58a
James Foster <github@jgfoster.net>
2021-04-26 14:08:27 -0700

    Refactor TemperatureControl. (#98)
    Co-authored-by: James Foster <git@jgfoster.net>

--- src/UIState/SetChillOrHeat.cpp
+++ src/UIState/SetChillOrHeat.cpp
@@ -4,15 +4,15 @@
 
 #include "SetChillOrHeat.h"
 
-#include "Devices/EEPROM_TC.h"
 #include "Devices/LiquidCrystal_TC.h"
+#include "Devices/TemperatureControl.h"
 
 void SetChillOrHeat::setValue(double value) {
   if (!(value == 1.0 || value == 9.0)) {
     LiquidCrystal_TC::instance()->writeLine("Invalid entry!  ", 1);
   } else {
     bool isHeat = (int)value == 9;
-    EEPROM_TC::instance()->setHeat(isHeat);
+    TemperatureControl::instance()->enableHeater(isHeat);
     if (isHeat) {
       LiquidCrystal_TC::instance()->writeLine("Use heater      ", 1);
     } else {

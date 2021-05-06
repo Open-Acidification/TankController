c9b026338cc0a62bb739255b5618fb8cae711e5a
James Foster <github@jgfoster.net>
2021-05-06 16:57:42 -0700

    Spelling

--- src/Devices/Serial_TC.cpp
+++ src/Devices/Serial_TC.cpp
@@ -61,7 +61,7 @@ void Serial_TC::vprintf(const char *format, va_list args) {
   if (!printIsActive) {
     printIsActive = true;
     // this seems to cause problems on the actual hardware
-    // SD_TC::instance()->appendToSerialLog(buffer);    // JGF
+    // SD_TC::instance()->appendToSerialLog(buffer);
     printIsActive = false;
   }
 }

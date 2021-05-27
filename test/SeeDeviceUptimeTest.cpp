986539b9b829aede1417da8cb398fff21a437a36
James Foster <github@jgfoster.net>
2021-05-26 17:08:20 -0700

    PushingBox Support (#166)
    * PushingBox works!

--- test/SeeDeviceUptimeTest.cpp
+++ test/SeeDeviceUptimeTest.cpp
@@ -11,7 +11,8 @@ unittest(testWaitState) {
   TankControllerLib* tc = TankControllerLib::instance();
   LiquidCrystal_TC* display = LiquidCrystal_TC::instance();
   assertEqual("MainMenu", tc->stateName());
-  delay(((((1 * 24 + 2) * 60) + 3) * 60 + 4) * 1000);  // 1 hr 2 minutes
+  GODMODE()->resetClock();
+  delay(((((1 * 24 + 2) * 60) + 3) * 60 + 4) * 1000);  // 1 day, 2 hours, 3 minutes, 4 seconds
   SeeDeviceUptime* test = new SeeDeviceUptime(tc);
   tc->setNextState(test, true);  // MainMenu -> SeeDeviceUptime nextState: Wait
   tc->loop();
@@ -22,6 +23,9 @@ unittest(testWaitState) {
   delay(1000);
   tc->loop();
   assertEqual(DateTime_TC::now().as16CharacterString(), display->getLines().at(0).c_str());
+  assertEqual("Up d:01 02:03:05", display->getLines().at(1));
+  delay(1000);
+  tc->loop();
   assertEqual("Up d:01 02:03:06", display->getLines().at(1));
   delay(1000);
   tc->loop();
@@ -35,9 +39,6 @@ unittest(testWaitState) {
   delay(1000);
   tc->loop();
   assertEqual("Up d:01 02:03:10", display->getLines().at(1));
-  delay(1000);
-  tc->loop();
-  assertEqual("Up d:01 02:03:11", display->getLines().at(1));
 }
 
 unittest_main()

eaa7ea1cb04f9138de432f5a3206a740e24ac87f
burkpa <burkpa@cs.wallawalla.edu>
2021-06-02 22:39:53 +0000

    Fix #117

--- src/UIState/SeePIDConstants.h
+++ src/UIState/SeePIDConstants.h
@@ -23,7 +23,8 @@ private:
   void loadKp(uint16_t line);
   void loadKi(uint16_t line);
   void loadKd(uint16_t line);
-  void loadSlope(uint16_t line);
+  void loadPID(uint16_t line);
+//   void loadSlope(uint16_t line);
 
   uint32_t startTime;
 };
--- test/SeePIDConstantsTest.cpp
+++ test/SeePIDConstantsTest.cpp
@@ -3,6 +3,7 @@
 
 #include "Keypad_TC.h"
 #include "LiquidCrystal_TC.h"
+#include "PHControl.h"
 #include "PID_TC.h"
 #include "SeePIDConstants.h"
 #include "TankControllerLib.h"
@@ -19,6 +20,7 @@ unittest(TestVerticalScroll) {
   float ki = pPID->getKi();
   float kd = pPID->getKd();
   pPID->setTunings(10001.0, 10002.0, 10003.0);
+  PHControl::instance()->enablePID(true);
 
   // Transition states
   assertEqual("MainMenu", tc->stateName());
@@ -36,11 +38,16 @@ unittest(TestVerticalScroll) {
   delay(2000);
   tc->loop();
   assertEqual("Kd: 10003.0     ", display->getLines().at(0));
-  assertEqual("                ", display->getLines().at(1));
+  assertEqual("PID: ON         ", display->getLines().at(1));
+  PHControl::instance()->enablePID(false);
   delay(3000);
   tc->loop();
   assertEqual("Kp: 10001.0     ", display->getLines().at(0));
   assertEqual("Ki: 10002.0     ", display->getLines().at(1));
+  delay(3000);
+  tc->loop();
+  assertEqual("Kd: 10003.0     ", display->getLines().at(0));
+  assertEqual("PID: OFF        ", display->getLines().at(1));
 
   Keypad_TC::instance()->_getPuppet()->push_back('D');
   tc->loop();

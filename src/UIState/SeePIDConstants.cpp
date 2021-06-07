dcc06de531a0dfecee76020a787bd70af0526fd3
Paydenb <83844058+Paydenb@users.noreply.github.com>
2021-06-07 15:46:11 -0700

    Fix #117 (#180)
    * Fix #117
    * remove commented out line

--- src/UIState/SeePIDConstants.cpp
+++ src/UIState/SeePIDConstants.cpp
@@ -5,6 +5,7 @@
 #include "SeePIDConstants.h"
 
 #include "Devices/LiquidCrystal_TC.h"
+#include "Devices/PHControl.h"
 #include "Devices/PHProbe.h"
 #include "Devices/PID_TC.h"
 #include "TC_util.h"
@@ -18,7 +19,7 @@ void SeePIDConstants::loop() {
       break;
     case 1:
       loadKd(0);
-      LiquidCrystal_TC::instance()->writeLine("               ", 1);
+      loadPID(1);
       break;
   }
 }
@@ -39,4 +40,13 @@ void SeePIDConstants::loadKd(uint16_t line) {
   char buffer[17];
   snprintf(buffer, sizeof(buffer), "Kd: %.1f", PID_TC::instance()->getKd());
   LiquidCrystal_TC::instance()->writeLine(buffer, line);
+}
+
+void SeePIDConstants::loadPID(uint16_t line) {
+  bool flag = PHControl::instance()->getUsePID();
+  if (flag) {
+    LiquidCrystal_TC::instance()->writeLine("PID: ON         ", line);
+  } else {
+    LiquidCrystal_TC::instance()->writeLine("PID: OFF        ", line);
+  }
 }

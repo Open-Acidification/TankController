e21b3c828266e026bdb3e4db96322bdf545eb112
James Foster <github@jgfoster.net>
2021-06-29 16:10:33 -0700

    Bump version to 21.06.2 (#208)
    Co-authored-by: James Foster <git@jgfoster.net>

--- src/TankControllerLib.cpp
+++ src/TankControllerLib.cpp
@@ -17,7 +17,7 @@
 #include "UIState/MainMenu.h"
 #include "UIState/UIState.h"
 
-const char TANK_CONTROLLER_VERSION[] = "21.06.1";
+const char TANK_CONTROLLER_VERSION[] = "21.06.2";
 
 // ------------ Class Methods ------------
 /**

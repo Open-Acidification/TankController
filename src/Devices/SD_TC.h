50fdee2ea1927d316cb38e21969eda1b1dd4fba7
James Foster <github@jgfoster.net>
2021-08-17 22:31:06 -0700

    Define `SS` for `SdFat` library (#239)
    * Fix #include reference so that we are using the new SdFat library.
    
    * Define `SS` before calling SdFat.h since it seems to expect that it was already defined.
    
    * Fix SD type name.
    
    * Start debugging out-of-memory issue.
    
    * Try singleton without format().
    
    * Add `format()` back.
    
    * Try again with debugging information in SdFat.
    
    * Add debugging to see if we get into `format()` function.
    
    * See if we can call any function (begin()) in SdFat.
    
    * Add debugging to SD_TC constructor and singleton accessor.
    
    * Did we initialize SdFat?
    
    * Install jgfoster/SdFat.git so we get debugging additions.
    
    * Use https to load library.
    
    * Try again after fixing `_didBegin` problem.
    
    * Reinstate original tests.
    
    * Remove debugging code.
    
    Co-authored-by: James Foster <git@jgfoster.net>

--- src/Devices/SD_TC.h
+++ src/Devices/SD_TC.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <Arduino.h>
-#include <SD.h>
+
+#define SS 4
+#include <SdFat.h>
 
 typedef void (*visitor)(File* pEntry, String parentPath);
 
@@ -26,9 +28,9 @@ private:
   static SD_TC* _instance;
 
   // instance variables
-  const uint8_t SD_SELECT_PIN = 4;
+  const uint8_t SD_SELECT_PIN = SS;
   bool hasHadError = false;
-  SD sd;
+  SdFat sd;
 
   // instance methods
   SD_TC();

494808714d8f77a091b7f22d2e9c3bf2a715ede7
James Foster <git@jgfoster.net>
2021-08-25 21:52:04 -0700

    Small refactorings.

--- src/UIState/SeeDeviceAddress.cpp
+++ src/UIState/SeeDeviceAddress.cpp
@@ -4,6 +4,8 @@
 
 #include "SeeDeviceAddress.h"
 
+#include <avr/wdt.h>
+
 #include "Devices/Ethernet_TC.h"
 #include "Devices/LiquidCrystal_TC.h"
 #include "Ethernet.h"
@@ -11,6 +13,8 @@
 void SeeDeviceAddress::handleKey(char key) {
   switch (key) {
     case 'B':  // Infinite loop to test Watchdog Timer
+      wdt_disable();
+      wdt_enable(WDTO_15MS);
       do {
       } while (true);
       break;
@@ -25,12 +29,13 @@ void SeeDeviceAddress::handleKey(char key) {
 }
 
 void SeeDeviceAddress::start() {
+  char line[17];
+  // first line
   IPAddress IP = Ethernet_TC::instance()->getIP();
+  snprintf(line, sizeof(line), "%03d.%03d.%03d.%03d", IP[0], IP[1], IP[2], IP[3]);
+  LiquidCrystal_TC::instance()->writeLine(line, 0);
+  // second line
   byte* mac = Ethernet_TC::instance()->getMac();
-  char line0[17];
-  char line1[17];
-  snprintf(line0, sizeof(line0), "%03d.%03d.%03d.%03d", IP[0], IP[1], IP[2], IP[3]);
-  snprintf(line1, sizeof(line1), "%02X%02X:%02X%02X:%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  LiquidCrystal_TC::instance()->writeLine(line0, 0);
-  LiquidCrystal_TC::instance()->writeLine(line1, 1);
+  snprintf(line, sizeof(line), "%02X%02X:%02X%02X:%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  LiquidCrystal_TC::instance()->writeLine(line, 1);
 }

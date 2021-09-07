c748c7c41a51d9cc5ed32d4f4e7c43b56326ff4d
James Foster <github@jgfoster.net>
2021-09-07 18:59:40 +0200

    Edit to remove backtick on shell script (as requested by Codacy).

--- GUI/build.sh
+++ GUI/build.sh
@@ -5,10 +5,10 @@ if [ ${PWD##*/} != "GUI" ]; then
 fi
 ../scripts/install_libraries.sh
 python3 -m pip install wxPython pybind11
-x=`bundle exec which arduino_library_location.rb`
-y=`echo ${x%/bin/*}/bundler/gems/arduino_ci-*`
+x=$(bundle exec which arduino_library_location.rb)
+y=$(echo "${x%/bin/*}/bundler/gems/arduino_ci-*")
 export ARDUINO_CI=${y}/cpp/arduino
-export TC_PATH=`(cd ..; pwd)`/src
+export TC_PATH=$((cd ..; pwd))/src
 make clean
 (mkdir -p .build; cd .build; make -f ../Makefile)
 python3 ./TankController.py

c6870aef2990d06c1d522fa5c58816479373028b
calvel <calvel@cs.wallawalla.edu>
2021-11-23 23:16:03 +0000

    change ramp output to correct output

--- src/UIState/SetPHSetPoint.cpp
+++ src/UIState/SetPHSetPoint.cpp
@@ -34,8 +34,7 @@ void SetPHSetPoint::setValue(float value) {
     char output1[17];
     snprintf_P(output0, sizeof(output0), (PGM_P)F("New pH=%i.%i"), (int)values[0],
                (int)(values[0] * 1000 + 0.5) % 1000);
-    snprintf_P(output1, sizeof(output1), (PGM_P)F("New ramp=%i.%i"), (int)values[1],
-               (int)(values[1] * 1000) % 1000);
+    snprintf_P(output1, sizeof(output1), (PGM_P)F("New ramp=%i.%i"), (int)values[1], (int)(values[1] * 1000) % 1000);
     LiquidCrystal_TC::instance()->writeLine(output0, 0);
     LiquidCrystal_TC::instance()->writeLine(output1, 1);
     returnToMainMenu(3000);  // after 3-second delay

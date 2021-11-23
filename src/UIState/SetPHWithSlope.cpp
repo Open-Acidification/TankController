de07b588fd461adfa5b936a86829463cdc572d75
calvel <calvel@cs.wallawalla.edu>
2021-11-23 23:04:17 +0000

    WIP

---
+++ src/UIState/SetPHWithSlope.cpp
@@ -0,0 +1,54 @@
+/**
+ * SetPHSetPoint.cpp
+ */
+
+#include "SetPHSetPoint.h"
+
+#include "Devices/LiquidCrystal_TC.h"
+#include "Devices/PHControl.h"
+
+SetPHSetPoint::SetPHSetPoint(TankController* tc) : NumCollectorState(tc) {
+  prompts[0] = F("Set pH Set Point");
+  prompts[1] = F("Set Amplitude:");
+  prompts[1] = F("Set Period:");
+}
+
+float SetPHSetPoint::getCurrentValue() {
+  if (subState == 0) {
+    return PHControl::instance()->getTargetPh();
+  } else if (subState == 1) {
+    if (PHControl::instance()->getPHSetType() == 2) {
+      return PHControl::instance()->getRampStartingPH();
+    } else {
+      return 0.000;
+    }
+  } else {
+    if (PHControl::instance()->getPHSetType() == 2) {
+      return PHControl::instance()->getRampStartingPH();
+    }
+    else {
+      return PHControl::instance()->getRampTimeStart() / 3600;
+    }
+  }
+}
+
+void SetPHSetPoint::setValue(float value) {
+  values[subState++] = value;
+  if (subState < NUM_VALUES) {
+    clear();
+    start();
+  } else {
+    PHControl::instance()->setTargetPh(values[0]);
+    PHControl::instance()->setSlope(values[1], values[2]);
+    char output0[17];
+    char output1[17];
+    char output2[17];
+    snprintf_P(output0, sizeof(output0), (PGM_P)F("New pH=%i.%i"), (int)values[0],
+               (int)(values[0] * 1000 + 0.5) % 1000);
+    snprintf_P(output1, sizeof(output1), (PGM_P)F("A=%i.%i"), (int)value, (int)(value * 1000) % 1000);
+    snprintf_P(output1, sizeof(output1), (PGM_P)F("P=%i.%i"), (int)value, (int)(value * 1000) % 1000);
+    LiquidCrystal_TC::instance()->writeLine(output0, 0);
+    LiquidCrystal_TC::instance()->writeLine(output1, 1);
+    returnToMainMenu(3000);  // after 3-second delay
+  }
+}

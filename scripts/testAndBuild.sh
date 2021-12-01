c6e84881e5aed8f94d0ae2b0f98fa5164a6e15fa
James Foster <github@jgfoster.net>
2021-11-30 21:07:19 -0800

    Update script to avoid unrecognized set option.

--- scripts/testAndBuild.sh
+++ scripts/testAndBuild.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 bundle config set --local path 'vendor/bundle'
 bundle install
-set -o pipefail
-bundle exec arduino_ci.rb --min-free-space=6000 | tee output.txt; result=$?
-set +o pipefail
+bundle exec arduino_ci.rb --min-free-space=6000 | tee output.txt
+result=${PIPESTATUS[0]}
 tail -n 4 output.txt | head -n 2 > size.txt
-exit "$result"
+exit $result

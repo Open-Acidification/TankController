b39a8a4450925a309bf5b6e9d287931efeb3132d
James Foster <github@jgfoster.net>
2021-11-30 20:41:15 -0800

    Update scripts so that errors are reported back to `arduino_ci`.

--- scripts/testAndBuild.sh
+++ scripts/testAndBuild.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 bundle config set --local path 'vendor/bundle'
 bundle install
-bundle exec arduino_ci.rb --min-free-space=6000 | tee output.txt
+set -o pipefail
+bundle exec arduino_ci.rb --min-free-space=6000 | tee output.txt; result=$?
+set +o pipefail
 tail -n 4 output.txt | head -n 2 > size.txt
+exit "$result"

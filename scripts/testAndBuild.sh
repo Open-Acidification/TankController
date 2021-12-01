7a98c0d9a4db4e1e70ecb55a03a017e81609ad9b
James Foster <github@jgfoster.net>
2021-11-30 22:32:18 -0800

    Try to fix "Bad substitution" error.

--- scripts/testAndBuild.sh
+++ scripts/testAndBuild.sh
@@ -2,6 +2,6 @@
 bundle config set --local path 'vendor/bundle'
 bundle install
 bundle exec arduino_ci.rb --min-free-space=6000 | tee output.txt
-result=${PIPESTATUS[0]}
+result="${PIPESTATUS[0]}"
 tail -n 4 output.txt | head -n 2 > size.txt
 exit "$result"

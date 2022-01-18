fe5866b19937a4495ea8a0713f5c9fe9bfa9e8ba
calvel <calvel@cs.wallawalla.edu>
2022-01-18 08:33:31 +0000

    WIP

--- src/Devices/PHControl.h
+++ src/Devices/PHControl.h
@@ -5,13 +5,13 @@
 class PHControl {
 private:
   // Class variable
-  static PHControl* _instance;
+  static PHControl *_instance;
   // instance variables
   uint32_t lastSwitchMS = 0;
   uint32_t lastWarnMS = 0;
   const uint16_t PH_CONTROL_PIN = 49;
   const uint16_t SOLENOID_OPENING_TIME = 100;
-  float targetPh;                      // actual target
+  float targetPh; // actual target
   float currentPHTarget;
   float rampStartingPh;
   uint32_t rampTimeStart;
@@ -19,7 +19,8 @@ private:
   float amplitude;
   uint32_t period;
   uint32_t sineStartTime;
-  const uint16_t WINDOW_SIZE = 10000;  // 10 second Proportional output window (for PID)
+  const uint16_t WINDOW_SIZE =
+      10000; // 10 second Proportional output window (for PID)
   bool usePID = true;
   int pHSetType = FLAT_TYPE;
   PHControl();
@@ -30,32 +31,16 @@ public:
     RAMP_TYPE,
     SINE_TYPE,
   };
-  static PHControl* instance();
+  static PHControl *instance();
   static void clearInstance();
-  float getTargetPh() {
-    return targetPh;
-  }
-  float getCurrentPhTarget() {
-    return currentPHTarget;
-  }
-  int getPhSetType() {
-    return pHSetType;
-  }
-  float getAmplitude() {
-    return amplitude;
-  }
-  uint32_t getPhRampTimeStart() {
-    return rampTimeStart;
-  }
-  uint32_t getPhRampTimeEnd() {
-    return rampTimeEnd;
-  }
-  uint32_t getPeriod() {
-    return period;
-  }
-  bool getUsePID() {
-    return usePID;
-  }
+  float getTargetPh() { return targetPh; }
+  float getCurrentPhTarget() { return currentPHTarget; }
+  int getPhSetType() { return pHSetType; }
+  float getAmplitude() { return amplitude; }
+  uint32_t getPhRampTimeStart() { return rampTimeStart; }
+  uint32_t getPhRampTimeEnd() { return rampTimeEnd; }
+  uint32_t getPeriod() { return period; }
+  bool getUsePID() { return usePID; }
   bool isOn();
   void setTargetPh(float newPh);
   void setRampDuration(float newPhRampDuration);

07bf6c17a771cf19ac3b6258bd1ba48cdf37b6af
natitr <natitr@cs.wallawalla.edu>
2022-02-06 06:27:39 +0000

    Fix test script

--- scripts/testAndBuild.sh
+++ scripts/testAndBuild.sh
@@ -3,5 +3,5 @@ bundle config set --local path 'vendor/bundle'
 bundle install
 bundle exec arduino_ci.rb --min-free-space=5960 | tee output.txt
 result="${PIPESTATUS[0]}"
-tail -n 4 output.txt | head -n 2 > size.txt
+tail -n 3 output.txt | head -n 1 > size.txt
 exit "$result"

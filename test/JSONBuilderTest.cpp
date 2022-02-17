86002efa4611b86bfbd6fd955d950d95fb66ec52
natitr <natitr@cs.wallawalla.edu>
2022-02-17 08:01:48 +0000

    Fix TC version in test

--- test/JSONBuilderTest.cpp
+++ test/JSONBuilderTest.cpp
@@ -28,7 +28,7 @@ unittest(current) {
       "\"PID\":\"ON\","
       "\"TankID\":0,"
       "\"Uptime\":\"0d 0h 0m 1s\","
-      "\"Version\":\"21.09.1\"}";
+      "\"Version\":\"22.02.1\"}";
   assertEqual(expected, text);
 }
 

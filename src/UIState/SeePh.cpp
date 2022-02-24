138930de4841a774c9581f4c399d6a8f59ddaad0
calvel <calvel@cs.wallawalla.edu>
2022-02-24 00:01:56 -0800

    update format

--- src/UIState/SeePh.cpp
+++ src/UIState/SeePh.cpp
@@ -62,7 +62,7 @@ void SeePh::loadTypeVariables(uint16_t line) {
       int timeLeftHours = timeLeft / 3600;
       int timeLeftSeconds = timeLeft % 60;
       snprintf_P(buffer, sizeof(buffer), (PGM_P)F("left: %i:%i:%i"), timeLeftHours, timeLeftMinutes,
-                  timeLeftSeconds);
+timeLeftSeconds);
       LiquidCrystal_TC::instance()->writeLine(buffer, line);
       break;
     }
@@ -93,7 +93,7 @@ void SeePh::loadValues(uint16_t line) {
   float currentTargetPh = PHControl::instance()->getCurrentPhTarget();
   float currentPh = PHProbe::instance()->getPh();
   snprintf_P(buffer, sizeof(buffer), (PGM_P)F("%i.%02i %i.%03i %i.%03i"), (int)currentPh,
-             (int)(currentPh * 100  + 0.5) % 100, (int)currentTargetPh, (int)(currentTargetPh * 1000 + 0.5) % 1000,
+             (int)(currentPh * 100 + 0.5) % 100, (int)currentTargetPh, (int)(currentTargetPh * 1000 + 0.5) % 1000,
              (int)overallTargetPh, (int)(overallTargetPh * 1000) % 1000);
   LiquidCrystal_TC::instance()->writeLine(buffer, line);
 }

25dfe54a488a3725620af645b777c8d0c793298b
calvel <calvel@cs.wallawalla.edu>
2022-03-08 11:11:25 -0800

    change to fill in 0's if needed

--- src/UIState/SetTempWithSine.cpp
+++ src/UIState/SetTempWithSine.cpp
@@ -45,10 +45,10 @@ void SetTempWithSine::setValue(float value) {
     char temp1[8];
     char temp2[9];
     char output1[17];
-    snprintf_P(output0, sizeof(output0), (PGM_P)F("New Temp=%i.%i"), (int)values[0],
+    snprintf_P(output0, sizeof(output0), (PGM_P)F("New Temp=%i.%03i"), (int)values[0],
                (int)(values[0] * 1000 + 0.5) % 1000);
-    snprintf_P(temp1, sizeof(temp1), (PGM_P)F("A=%i.%i"), (int)values[1], (int)(values[1] * 1000) % 1000);
-    snprintf_P(temp2, sizeof(temp2), (PGM_P)F(" P=%i.%i"), (int)values[2], (int)(values[2] * 1000) % 1000);
+    snprintf_P(temp1, sizeof(temp1), (PGM_P)F("A=%i.%03i"), (int)values[1], (int)(values[1] * 1000) % 1000);
+    snprintf_P(temp2, sizeof(temp2), (PGM_P)F(" P=%i.%03i"), (int)values[2], (int)(values[2] * 1000) % 1000);
     snprintf_P(output1, sizeof(output1), (PGM_P)F("%s%s"), (const char*)temp1, (const char*)temp2);
     LiquidCrystal_TC::instance()->writeLine(output0, 0);
     LiquidCrystal_TC::instance()->writeLine(output1, 1);

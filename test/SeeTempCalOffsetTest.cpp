7867a65ad6986fe67eeea88cb58f3c0c7e82cca0
pianistrevor <trevornatiuk@gmail.com>
2022-03-15 19:39:59 -0700

    Test coverage for menu

---
+++ test/SeeTempCalOffsetTest.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <ArduinoUnitTests.h>
+
+#include "Keypad_TC.h"
+#include "LiquidCrystal_TC.h"
+#include "SeeTempCalOffset.h"
+#include "TankController.h"
+
+unittest(testOutput) {
+  // Set up
+  TankController* tc = TankController::instance();
+  LiquidCrystal_TC* display = LiquidCrystal_TC::instance();
+  assertEqual("MainMenu", tc->stateName());
+  SeeTempCalOffset* test = new SeeTempCalOffset(tc);
+  tc->setNextState(test, true);
+  assertEqual("SeeTempCalOffset", tc->stateName());
+
+  // Test the prompt
+  assertEqual("Temp Cal Offset:", display->getLines().at(0));
+  // Check value
+  assertEqual("0.00000         ", display->getLines().at(1));
+  // Return to mainMenu
+  Keypad_TC::instance()->_getPuppet()->push_back('D');
+  tc->loop();
+  assertEqual("MainMenu", tc->stateName());
+}
+
+unittest_main()

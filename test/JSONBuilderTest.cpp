23437d3d167c7c5b6f1cf594a635283e115677b3
James Foster <github@jgfoster.net>
2022-03-21 14:33:01 -0700

    * Allow Cross Origin Resource Sharing (CORS). That is, anyone who wants to get to the device may do so. * Update version.

--- test/JSONBuilderTest.cpp
+++ test/JSONBuilderTest.cpp
@@ -30,7 +30,7 @@ unittest(current) {
       "\"PID\":\"ON\","
       "\"TankID\":0,"
       "\"Uptime\":\"0d 0h 0m 1s\","
-      "\"Version\":\"22.03.1\"}";
+      "\"Version\":\"22.03.2\"}";
   assertEqual(expected, text);
 }
 

2d3c56df5c568245b4eca2a74905aa4450f4f04b
calvel <calvel@cs.wallawalla.edu>
2022-06-16 13:40:02 -0700

    fix failing tests

--- src/TankController.cpp
+++ src/TankController.cpp
@@ -21,7 +21,7 @@
 #include "UIState/MainMenu.h"
 #include "UIState/UIState.h"
 
-const char TANK_CONTROLLER_VERSION[] = "22.04.1         ";
+const char TANK_CONTROLLER_VERSION[] = "22.04.1";
 
 // ------------ Class Methods ------------
 /**

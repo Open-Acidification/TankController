69215d531116662208636cd044f446215ecec79d
James Foster <git@jgfoster.net>
2022-07-06 09:37:41 -0700

    Revert "Fix MainMenu excess code; add tests for read/write speeds (#310)"
    This reverts commit b4d880e0f02c659af4ef2963c1ec11a7885e301e.

--- src/Devices/EthernetServer_TC.h
+++ src/Devices/EthernetServer_TC.h
@@ -9,7 +9,7 @@
 #include <Ethernet.h>
 #endif
 
-enum serverState_t { NOT_CONNECTED, READ_REQUEST, GET_REQUEST, POST_REQUEST, IN_PROGRESS, IN_TRANSFER, FINISHED };
+enum serverState_t { NOT_CONNECTED, READ_REQUEST, GET_REQUEST, POST_REQUEST, IN_PROGRESS, FINISHED };
 
 /**
  * EthernetServer_TC provides wrapper for web server for TankController
@@ -28,7 +28,7 @@ public:
     return state;
   }
   void loop();
-  void writeToClientBuffer(char*, bool);
+  void writeBufferToClient(char*, bool);
 
 private:
   // class variables
@@ -38,32 +38,23 @@ private:
   EthernetClient client;
   serverState_t state = NOT_CONNECTED;
   char buffer[512];
-  char boundary[9];
   unsigned int bufferContentsSize = 0;
   unsigned long connectedAt = 0;
-  File file;
-  int startTime;
 
   // instance methods: constructor
   EthernetServer_TC(uint16_t port);
   // instance methods: utility
   void sendHeadersWithSize(uint32_t size);
-  void sendFileHeadersWithSize(uint32_t size);
   void sendRedirectHeaders();
   void sendBadRequestHeaders();
   int weekday(int year, int month, int day);
   // instance methods: HTTP
-  void get();
-  void post();
   void echo();
-  void apiHandler();
   void current();
   void display();
   void keypress();
   void rootdir();
-  void testReadSpeed();
-  void testWriteSpeed();
-  bool isFileRequest();
-  void fileSetup();
-  bool fileContinue();
+  bool file();
+  void get();
+  void post();
 };
--- src/TankController.cpp
+++ src/TankController.cpp
@@ -143,7 +143,7 @@ void TankController::handleUI() {
 /**
  * This is one of two public instance functions.
  * It is called repeatedly while the board is on.
- * (loop() appears to finish in 2-4 ms idling at main menu.)
+ * (It appears to be called about once every 15 ms.)
  */
 void TankController::loop() {
   wdt_reset();

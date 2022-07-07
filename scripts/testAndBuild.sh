85af5752a8e0908caf616380621403fdd4560bca
James Foster <git@jgfoster.net>
2022-07-06 17:01:09 -0700

    * Update size: it seems that gcc version 9.4.0 (Ubuntu 9.4.0-1ubuntu1~20.04.1) uses 32 bytes more than Apple clang version 12.0.0 (clang-1200.0.32.29). * Add script to build without testing

--- scripts/testAndBuild.sh
+++ scripts/testAndBuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bundle config set --local path 'vendor/bundle'
 bundle install
-bundle exec arduino_ci.rb --min-free-space=5860 1> >(tee stdout.txt ) 2> >(tee stderr.txt >&2 )
+bundle exec arduino_ci.rb --min-free-space=5830 1> >(tee stdout.txt ) 2> >(tee stderr.txt >&2 )
 result="${PIPESTATUS[0]}"
 tail -n 3 stdout.txt | head -n 1 > size.txt
 exit "$result"

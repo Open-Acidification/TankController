cbabaa4dafc8d49b3f36a41904626eab7e53ab39
calvel <calvel@cs.wallawalla.edu>
2022-07-11 16:38:20 -0700

    fix failing test

--- test/MenuTest.cpp
+++ test/MenuTest.cpp
@@ -89,8 +89,8 @@ unittest(ViewSettings) {
   assertEqual("<4   ^2  8v   6>", lc->getLines().at(1));
   enterKey('D');
   tc->loop();
-  assertEqual("pH 0.000   8.100", lc->getLines().at(0));
-  assertEqual("T 12.23 H 15.75 ", lc->getLines().at(1));
+  assertEqual("pH=0.000   8.100", lc->getLines().at(0));
+  assertEqual("T=12.23 H 15.75 ", lc->getLines().at(1));
 }
 
 unittest(SetPHSetPoint) {

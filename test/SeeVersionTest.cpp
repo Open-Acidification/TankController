077d7472dfe98777b05f3b08c3f09546f845b863
James Foster <github1@jgfoster.net>
2022-07-25 16:54:34 -0700

    Update version number to 22.07.2 (fix #323). (#324)
    Co-authored-by: James Foster <github@jgfoster.net>

--- test/SeeVersionTest.cpp
+++ test/SeeVersionTest.cpp
@@ -17,7 +17,7 @@ unittest(testOutput) {
   // Test the output
   tc->loop();
   assertEqual("Software Version", display->getLines().at(0));
-  assertEqual("22.07.1         ", display->getLines().at(1));
+  assertEqual("22.07.2         ", display->getLines().at(1));
   // Return to mainMenu
   Keypad_TC::instance()->_getPuppet()->push_back('D');
   tc->loop();

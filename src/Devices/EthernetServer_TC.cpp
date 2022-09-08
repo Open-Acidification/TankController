8bff60e1af3862913a55a8a354ba48243a94062a
John Foster <john@jefoster.net>
2022-09-08 15:51:53 -0700

    Minor changes

--- src/Devices/EthernetServer_TC.cpp
+++ src/Devices/EthernetServer_TC.cpp
@@ -342,7 +342,7 @@ void EthernetServer_TC::sampleSetup() {
   //   // Skip header row.
   //   int readSize = file.fgets(buffer, sizeof(buffer), delim);
   // }
-  file.seekSet(61);           // Skip header row
+  file.seekSet(63);           // Skip header row
   sendHeadersWithSize(1320);  // 60 lines, 22 characters per line
   serial(F("Header has been sent"));
 }

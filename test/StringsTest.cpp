2ac9de3df5da70e028fb06bef9133559e70f3228
John Foster <john@jefoster.net>
2022-10-21 12:44:04 -0700

    attempt5

--- test/StringsTest.cpp
+++ test/StringsTest.cpp
@@ -93,9 +93,10 @@ unittest(floatToString) {
   assertEqual(1, error_code);
   assertEqual("-1000.", buffer);
   serialOutput = state->serialPort[0].dataOut;
-  // int16_t j = serialOutput.indexOf('\r');
-  assertEqual("WARNING! String \"-1000.44\" was truncated to \"-1000.\"\r\n", serialOutput.c_str());
-  // assertEqual("WARNING! String \"-1000.44\" was truncated to \"-1000.\"", serialOutput.substring(0, j).c_str());
+  int16_t j = serialOutput.indexOf('\r');
+  // assertEqual("WARNING! String \"-1000.44\" was truncated to \"-1000.\"\r\n", serialOutput.c_str());
+  assertEqual("WARNING! String \"-1000.44\" was truncated to \"-1000.\"", serialOutput.substring(0, j).c_str());
+  COUT << serialOutput.c_str();
 
   // String contents(data), line;
   // int16_t i = contents.indexOf('\n');
@@ -109,6 +110,7 @@ unittest(floatToString) {
   assertEqual("1.3000", buffer);
   serialOutput = state->serialPort[0].dataOut;
   assertEqual("WARNING! String \"1.30000\" was truncated to \"1.3000\"\r\n", serialOutput.c_str());
+  COUT << serialOutput.c_str();
 
   state->serialPort[0].dataOut = "";
   num = 10000000000000.0;
@@ -117,6 +119,7 @@ unittest(floatToString) {
   assertEqual("100000", buffer);
   serialOutput = state->serialPort[0].dataOut;
   assertEqual("WARNING! Overflow may have occurred before truncating to \"100000\"\r\n", serialOutput.c_str());
+  COUT << serialOutput.c_str();
 }
 
 unittest_main()

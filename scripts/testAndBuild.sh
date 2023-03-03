c940c63923552d039c6c66a1fb5130430cede16e
James Foster <git@jgfoster.net>
2023-03-02 20:38:16 -0800

    Tests work in local environment.

--- scripts/testAndBuild.sh
+++ scripts/testAndBuild.sh
@@ -3,5 +3,5 @@ bundle config set --local path 'vendor/bundle'
 bundle install
 bundle exec arduino_ci.rb --min-free-space=5830 1> >(tee stdout.txt ) 2> >(tee stderr.txt >&2 )
 result="${PIPESTATUS[0]}"
-tail -n 3 stdout.txt | head -n 1 > size.txt
+tail -n 2 stdout.txt | head -n 1 > size.txt
 exit "$result"

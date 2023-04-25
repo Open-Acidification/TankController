69215eca955e88b160856b0efb2da1f0ed75a6b0
Evan Taylor <suocesky@gmail.com>
2023-04-25 14:47:15 -0700

    Minor changes

--- extras/webClient/lib/view/information_page.dart
+++ extras/webClient/lib/view/information_page.dart
@@ -15,94 +15,93 @@ class Information extends StatelessWidget {
   }) : super(key: key);
 
   final BuildContext context;
+}
 
-  void handleResult(Object result, String ip) async {
-    Uint8List bytesData =
-        const Base64Decoder().convert(result.toString().split(',').last);
-    List<int> selectedFile = bytesData;
-    await makeRequest(ip, selectedFile);
-  }
+void handleResult(Object result, String ip) async {
+  Uint8List bytesData =
+      const Base64Decoder().convert(result.toString().split(',').last);
+  List<int> selectedFile = bytesData;
+  await makeRequest(ip, selectedFile);
+}
 
-  Future<String?> makeRequest(String ip, List<int> selectedFile) async {
-    var url = Uri.parse(ip);
-    var request = http.MultipartRequest('POST', url);
-    request.files.add(
-      http.MultipartFile.fromBytes(
-        'file',
-        selectedFile,
-        contentType: MediaType('application', 'octet-stream'),
-        filename: 'file_up',
-      ),
-    );
-    var res = await request.send();
-    return res.reasonPhrase;
-    }
-    //Can I copy Dialog box frame from Shortcuts?
-  }
+Future<String?> makeRequest(String ip, List<int> selectedFile) async {
+  var url = Uri.parse(ip);
+  var request = http.MultipartRequest('POST', url);
+  request.files.add(
+    http.MultipartFile.fromBytes(
+      'file',
+      selectedFile,
+      contentType: MediaType('application', 'octet-stream'),
+      filename: 'file_up',
+    ),
+  );
+  var res = await request.send();
+  return res.reasonPhrase;
+}
+//Can I copy Dialog box frame from Shortcuts?
 
-  startWebFilePicker(String ip) async {
-    html.FileUploadInputElement uploadInput = html.FileUploadInputElement();
-    uploadInput.multiple = true;
-    uploadInput.draggable = true;
-    uploadInput.click();
-    uploadInput.onChange.listen((e) {
-      final files = uploadInput.files;
-      final file = files![0];
-      dynamic reader = html.FileReader();
-      reader.onLoadEnd.listen((e) {
-        handleResult(reader.result, ip);
-      });
-      reader.readAsDataUrl(file);
+startWebFilePicker(String ip) async {
+  html.FileUploadInputElement uploadInput = html.FileUploadInputElement();
+  uploadInput.multiple = true;
+  uploadInput.draggable = true;
+  uploadInput.click();
+  uploadInput.onChange.listen((e) {
+    final files = uploadInput.files;
+    final file = files![0];
+    dynamic reader = html.FileReader();
+    reader.onLoadEnd.listen((e) {
+      handleResult(reader.result, ip);
     });
-  }
+    reader.readAsDataUrl(file);
+  });
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Consumer<AppData>(
-        builder: (context, appData, child) {
-          var informationRows = <DataRow>[];
-          appData.information.forEach(
-            (key, value) => informationRows.add(
-              DataRow(
-                cells: <DataCell>[
-                  DataCell(Text(key.toString())),
-                  DataCell(Text(value.toString()))
+@override
+Widget build(BuildContext context) {
+  return Container(
+    color: Colors.white,
+    child: Consumer<AppData>(
+      builder: (context, appData, child) {
+        var informationRows = <DataRow>[];
+        appData.information.forEach(
+          (key, value) => informationRows.add(
+            DataRow(
+              cells: <DataCell>[
+                DataCell(Text(key.toString())),
+                DataCell(Text(value.toString()))
+              ],
+            ),
+          ),
+        );
+        return Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  DataTable(
+                    headingRowHeight: 0,
+                    columns: const <DataColumn>[
+                      DataColumn(
+                        label: Text('Key'),
+                      ),
+                      DataColumn(
+                        label: Text('Value'),
+                      ),
+                    ],
+                    rows: informationRows,
+                  ),
                 ],
               ),
             ),
-          );
-          return Column(
-            children: [
-              Expanded(
-                child: ListView(
-                  children: <Widget>[
-                    DataTable(
-                      headingRowHeight: 0,
-                      columns: const <DataColumn>[
-                        DataColumn(
-                          label: Text('Key'),
-                        ),
-                        DataColumn(
-                          label: Text('Value'),
-                        ),
-                      ],
-                      rows: informationRows,
-                    ),
-                  ],
-                ),
-              ),
-              OutlinedButton(
-                onPressed: () {
-                  startWebFilePicker(appData.currentTank.ip.toString());
-                },
-                child: const Text('Add File'),
-              )
-            ],
-          );
-        },
-      ),
-    );
-  }
+            OutlinedButton(
+              onPressed: () {
+                startWebFilePicker(appData.currentTank.ip.toString());
+              },
+              child: const Text('Add File'),
+            )
+          ],
+        );
+      },
+    ),
+  );
 }

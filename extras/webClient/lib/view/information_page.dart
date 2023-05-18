d03721520da3e3e10de09c0014d34f779ad84a4b
Suocesky <73154821+Suocesky@users.noreply.github.com>
2023-05-18 16:19:39 -0700

    Preparing support for an arbitrary path defined in an uploaded file (#386)
    * Unable to add a button so far
    
    * Basic add file function w/o uploads
    
    * Continued work on file uploads
    
    * Continued work on file uploads
    
    * Progress on File Upload
    
    * More work on File Upload
    
    * Finished File Upload for now
    
    * Minor changes
    
    * File add changes
    
    * Finish Add Files
    
    * Lint fix
    
    * Lint fixes
    
    * Added version check to AddFiles function
    
    * Try a format fix
    
    * Revert accidental change
    
    * Format fix
    
    * Merge main into branch
    
    * Added file filter and file size limit
    
    * Minor lint fix
    
    * Fix html library package warning
    
    * Temporary lint fix
    
    * Fix testing issue for html
    
    * A second html fix
    
    * Format fix
    
    * Changed editable fields based on feedback from Dr. Onthank
    
    * Removed rtf from accepted files for upload
    
    * Removed extra comma

--- extras/webClient/lib/view/information_page.dart
+++ extras/webClient/lib/view/information_page.dart
@@ -1,3 +1,10 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:tank_manager/view/mock_html.dart'
+    if (dart.library.html) 'dart:html' as html;
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tank_manager/model/app_data.dart';
@@ -13,10 +20,12 @@ class Information extends StatelessWidget {
   final BuildContext context;
 
   bool showEdit(var valueString) {
-    return valueString == 'PHSlope' ||
+    return valueString == 'GoogleSheetInterval' ||
         valueString == 'Kp' ||
         valueString == 'Ki' ||
-        valueString == 'Kd';
+        valueString == 'Kd' ||
+        valueString == 'PID' ||
+        valueString == 'TankID';
   }
 
   bool versionCheck(var versionValue) {
@@ -63,6 +72,74 @@ class Information extends StatelessWidget {
     );
   }
 
+  showPopupDialog(String titleString, String messageString) async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(titleString),
+          content: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    messageString,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void handleResult(Object result, String ip) async {
+    Uint8List bytesData =
+        const Base64Decoder().convert(result.toString().split(',').last);
+    if (bytesData.length > 10000) {
+      throw UnsupportedError(
+        showPopupDialog('File too large', 'Your file exceeds 10 KB.'),
+      );
+    }
+    List<int> selectedFile = bytesData;
+    await makeRequest(ip, selectedFile);
+  }
+
+  Future<String?> makeRequest(String ip, List<int> selectedFile) async {
+    var url = Uri.parse(ip);
+    var request = http.MultipartRequest('POST', url);
+    request.files.add(
+      http.MultipartFile.fromBytes(
+        'file',
+        selectedFile,
+        contentType: MediaType('application', 'octet-stream'),
+        filename: 'file_up',
+      ),
+    );
+    var res = await request.send();
+    return res.reasonPhrase;
+  }
+
+  startWebFilePicker(String ip) async {
+    html.FileUploadInputElement uploadInput = html.FileUploadInputElement();
+    uploadInput.multiple = true;
+    uploadInput.draggable = true;
+    uploadInput.accept = '.txt,.csv';
+    uploadInput.click();
+    uploadInput.onChange.listen((e) {
+      final files = uploadInput.files;
+      final file = files![0];
+      dynamic reader = html.FileReader();
+      reader.onLoadEnd.listen((e) {
+        handleResult(reader.result, ip);
+      });
+      reader.readAsDataUrl(file);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -89,20 +166,50 @@ class Information extends StatelessWidget {
               ),
             ),
           );
-          return ListView(
-            children: <Widget>[
-              DataTable(
-                headingRowHeight: 0,
-                columns: const <DataColumn>[
-                  DataColumn(
-                    label: Text('Key'),
-                  ),
-                  DataColumn(
-                    label: Text('Value'),
-                  ),
-                ],
-                rows: informationRows,
-              )
+          return Column(
+            children: [
+              Expanded(
+                child: ListView(
+                  children: <Widget>[
+                    DataTable(
+                      headingRowHeight: 0,
+                      columns: const <DataColumn>[
+                        DataColumn(
+                          label: Text('Key'),
+                        ),
+                        DataColumn(
+                          label: Text('Value'),
+                        ),
+                      ],
+                      rows: informationRows,
+                    ),
+                  ],
+                ),
+              ),
+              // This outer ternary is required to prevent the screen flashing an error
+              // for a split second while appData.information loads to do the check.
+              (appData.information['Version'] != null)
+                  ? OutlinedButton(
+                      onPressed: versionCheck(appData.information['Version'])
+                          ? () {
+                              unawaited(
+                                startWebFilePicker(
+                                  appData.currentTank.ip.toString(),
+                                ),
+                              );
+                            }
+                          : () {
+                              showPopupDialog(
+                                'Feature coming soon',
+                                'Your tank controller is not at a version that supports file upload',
+                              );
+                            },
+                      child: const Text('Upload file for arbitrary path'),
+                    )
+                  : OutlinedButton(
+                      onPressed: () {},
+                      child: const Text('Upload file for arbitrary path'),
+                    )
             ],
           );
         },

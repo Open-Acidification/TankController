8c57de7c9a3f16a344e080578299ac051e09737b
John Foster <john@jefoster.net>
2023-07-15 22:13:56 -0700

    WIP tests

---
+++ test/SeePHCalibrationTest.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <ArduinoUnitTests.h>
+
+#include "Keypad_TC.h"
+#include "LiquidCrystal_TC.h"
+#include "SeePHCalibration.h"
+#include "TankController.h"
+
+unittest(testOutput) {
+  // Set up
+  TankController* tc = TankController::instance();
+  LiquidCrystal_TC* display = LiquidCrystal_TC::instance();
+  PHProbe* pPHProbe = PHProbe::instance();
+  char buffer[20];
+  pPHProbe->setCalibrationPoints(3);
+
+  assertEqual("MainMenu", tc->stateName());
+  SeePHSlope* test = new SeePHSlope(tc);
+  tc->setNextState(test, true);
+  assertEqual("SeePHSlope", tc->stateName());
+
+  // Test the output
+  assertEqual("PH Slope:       ", display->getLines().at(0));
+  assertEqual("requesting slope", display->getLines().at(1));
+  tc->loop(false);
+  assertEqual("Requesting...   ", display->getLines().at(1));
+  tc->loop(false);
+  assertEqual("3 point         ", display->getLines().at(1));
+  // Return to mainMenu
+  Keypad_TC::instance()->_getPuppet()->push_back('D');
+  tc->loop(false);
+  assertEqual("MainMenu", tc->stateName());
+}
+
+unittest_main()

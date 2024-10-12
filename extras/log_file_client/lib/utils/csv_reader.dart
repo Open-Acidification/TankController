83bbe597280119227168ade660b326c429ac9f07
Anna Stefaniv <happyasme11@gmail.com>
2024-10-12 16:11:56 -0700

    CsvReader now can output nested list of formatted data types

--- extras/log_file_client/lib/utils/csv_reader.dart
+++ extras/log_file_client/lib/utils/csv_reader.dart
@@ -9,17 +9,63 @@ abstract class CsvReader {
   CsvReader(this.filePath);
 
   Future<String> fetchCsvData();
+
+  String formatDateString(String dateString) {
+    final parts = dateString.split(RegExp('[ /:]'));
+    if (parts.length == 6) {
+      final formattedDateString = '${parts[2]}-${parts[0]}-${parts[1]} ${parts[3]}:${parts[4]}:${parts[5]}';
+      return formattedDateString;
+    } else {
+      throw Exception('Unable to format date string.');
+    }
+  }
+
+  Future<List<List>> csvTable() {
+    return fetchCsvData().then((String data) {
+      final List<List> table = [];
+
+      final List<String> rows = data.split('\n');
+      for (int i = 0; i < rows.length; i++) {
+        final List<String> stringCells = rows[i].replaceAll('\r', '').split(',');
+
+        // Convert from strings to useful types
+        List<dynamic> cells = List.generate(stringCells.length, (int j) {
+          if (i == 0) {
+            return stringCells[j];
+          } else {
+            switch (j) {
+              case 0:
+                return DateTime.tryParse(formatDateString(stringCells[j])) ?? stringCells[j];
+              case 1:
+              case 6:
+                return int.tryParse(stringCells[j]) ?? stringCells[j];
+              case 2:
+              case 3:
+              case 4:
+              case 5:
+              case 7:
+              case 8:
+              case 9:
+                return double.tryParse(stringCells[j]) ?? stringCells[j];
+            }
+          }
+        });
+
+        table.add(cells);
+      }
+
+      return table;
+    });
+  }
   
 }
 
-// Subclass CsvReaderForTest
 class CsvReaderForTest extends CsvReader {
   CsvReaderForTest(String filePath) : super(filePath);
 
   @override
   Future<String> fetchCsvData() async {
     try {
-      // Read the file from the local file system asynchronously
       final file = File(filePath);
       final String contents = await file.readAsString();
       return contents;
@@ -38,49 +84,9 @@ class CsvReaderForApp extends CsvReader {
     final http.Response response = await http.get(url);
 
     if (response.statusCode == 200) {
-      return response.body; // Return the raw CSV data
+      return response.body;
     } else {
       throw Exception('Failed to load CSV data');
     }
   }
 }
-
-
-// import 'package:http/http.dart' as http;
-
-// abstract class CsvReader {
-//   final String filePath;
-//   CsvReader(this.filePath);
-
-//   Future<String> fetchCsvData();
-
-
-// }
-
-// class CsvReaderForTest implements CsvReader {
-//   CsvReaderForTest(String filePath) : super(filePath);
-
-//   @override
-//   Future<String> fetchCsvData() async {
-//     // Implement test-specific CSV fetching logic here
-//     return 'Test CSV data';
-//   }
-// }
-
-// class CsvReaderForApp implements CsvReader {
-//   CsvReaderForApp(this.filePath);
-//   @override
-//   final String filePath;
-
-  // @override
-  // Future<String> fetchCsvData() async {
-  //   final url = Uri.https('oap.cs.wallawalla.edu', filePath);
-  //   final http.Response response = await http.get(url);
-
-  //   if (response.statusCode == 200) {
-  //     return response.body; // Return the raw CSV data
-  //   } else {
-  //     throw Exception('Failed to load CSV data');
-  //   }
-  // }
-// }
--- extras/log_file_client/test/csv_reader_test.dart
+++ extras/log_file_client/test/csv_reader_test.dart
@@ -20,7 +20,7 @@ void main() {
   });
 
   test('fetchCsvData with app class', () async {
-    final reader = CsvReaderForApp('/logs/20230120.csv'); // Update the file path as needed
+    final reader = CsvReaderForApp('/logs/20230120.csv');
     final data = await reader.fetchCsvData();
 
     // Split the lines to not have to check all of them
@@ -30,6 +30,35 @@ void main() {
     expect(lines[0], 'time,tankid,temp,temp setpoint,pH,pH setpoint,onTime,Kp,Ki,Kd');
     expect(lines[1], '01/20/2023 16:18:21,  99, 0.00, 11.00, 0.000, 8.645,    6,    700.0,    100.0,      0.0');
     expect(lines[2], '01/20/2023 16:18:22,  99, 0.00, 11.00, 0.000, 8.645,    8,    700.0,    100.0,      0.0');
+  });
+
+  test('formatDateString', () {
+    final reader = CsvReaderForTest('test');
+    
+    final data1 = reader.formatDateString('01/20/2023 16:18:21');
+    expect(data1, equals('2023-01-20 16:18:21'));
+
+    final data2 = reader.formatDateString('02/15/2021 14:22:45');
+    expect(data2, equals('2021-02-15 14:22:45'));
+
+    final data3 = reader.formatDateString('03/10/2022 09:30:12');
+    expect(data3, equals('2022-03-10 09:30:12'));
+
+    final data4 = reader.formatDateString('04/05/2020 18:45:33');
+    expect(data4, equals('2020-04-05 18:45:33'));
+  });
+
+  test('csvTable', () async {
+    final reader = CsvReaderForTest('test/test_assets/csv_reader_test.csv');
+    final data = await reader.csvTable();
+    expect(data, 
+    [['time','tankid','temp','temp setpoint','pH','pH setpoint','onTime','Kp','Ki','Kd'],
+    [DateTime.parse('2023-01-20 16:18:21'), 99, 0.00, 10.00, 0.000, 8.645, 6, 700.0, 100.0, 0.0],
+    [DateTime.parse('2023-01-20 16:18:22'), 98, 1.23, 10.00, 7.123, 8.645, 8, 710.0, 110.0, 1.0],
+    [DateTime.parse('2023-01-20 16:18:23'), 97, 2.34, 10.00, 6.789, 8.645, 9, 720.0, 120.0, 2.0],
+    [DateTime.parse('2023-01-20 16:18:24'), 96, 3.45, 10.00, 5.456, 8.645, 10, 730.0, 130.0, 3.0],
+    [DateTime.parse('2023-01-20 16:18:25'), 95, 4.56, 10.00, 4.123, 8.645, 11, 740.0, 140.0, 4.0]
+    ],);
   });
 
 }

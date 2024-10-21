5c8fcd79462dfabfab991e7664ea608f37ddfa12
Anna Stefaniv <happyasme11@gmail.com>
2024-10-21 15:04:00 -0700

    Set up log file server for working with proxy server

--- extras/log_file_server/bin/log_file_server.dart
+++ extras/log_file_server/bin/log_file_server.dart
@@ -5,7 +5,7 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-String rootDir = '/var/opt/oap/logs';
+String rootDir = Platform.localHostname == 'oap-vm' ? '/var/opt/oap/logs' : './test/logs';
 
 // Configure routes.
 final _router = Router()
@@ -111,7 +111,7 @@ void main(List<String> args) async {
       Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
 
   // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final port = int.parse(Platform.environment['PORT'] ?? '8082');
   final server = await serve(handler, ip, port);
   print('Server listening on port ${server.port}');
 }

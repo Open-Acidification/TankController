743a2c86e846b9274a2cc2a5945aa9045ece01a6
Anna Stefaniv <happyasme11@gmail.com>
2025-02-04 16:52:00 -0800

    Removed redirect button - web package interferes with flutter tests

--- extras/device_client/lib/view/home_page.dart
+++ extras/device_client/lib/view/home_page.dart
@@ -7,7 +7,7 @@ import 'package:device_client/view/current_data_page.dart';
 import 'package:device_client/view/files_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:web/web.dart' as html;
+// import 'package:web/web.dart' as html;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({required this.title, super.key});
@@ -46,9 +46,9 @@ class _MyHomePageState extends State<MyHomePage> {
             title: Text(
               '${widget.title}: ${appData.currentTank.name}',
             ),
-            actions: [
-              _tankMonitorRedirect(),
-            ],
+            // actions: [
+            //   _tankMonitorRedirect(),
+            // ],
           ),
           drawer: AppDrawer(context: context),
           body: children[appData.currentIndex],
@@ -58,26 +58,26 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-    Padding _tankMonitorRedirect() {
-    return Padding(
-      padding: const EdgeInsets.only(right: 8.0),
-      child: TextButton(
-        onPressed: () {
-          const tankMonitorUrl = 'https://oap.cs.wallawalla.edu/';
-          html.window.open(tankMonitorUrl, '_self');
-        },
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: const [
-            Text(
-              'Go to Tank Monitor',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(width: 8),
-            Icon(Icons.swap_horiz),
-          ],
-        ),
-      ),
-    );
-  }
+  //   Padding _tankMonitorRedirect() {
+  //   return Padding(
+  //     padding: const EdgeInsets.only(right: 8.0),
+  //     child: TextButton(
+  //       onPressed: () {
+  //         const tankMonitorUrl = 'https://oap.cs.wallawalla.edu/';
+  //         html.window.open(tankMonitorUrl, '_self');
+  //       },
+  //       child: Row(
+  //         mainAxisSize: MainAxisSize.min,
+  //         children: const [
+  //           Text(
+  //             'Go to Tank Monitor',
+  //             style: TextStyle(fontWeight: FontWeight.bold),
+  //           ),
+  //           SizedBox(width: 8),
+  //           Icon(Icons.swap_horiz),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
